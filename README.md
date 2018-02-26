1dff943eef37294e6b948013e9937ef4f100c72f
sleeplesseditor <josh@bryson.me.uk>
2018-02-26 12:02:28 +0000

    Google OAuth setup and Nodemon installed

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a fullstack app built using React, Redux, Node, Express, Google OAuth, PassportJS, and MongoDB for collecting feedback, and payment handling via Stripe, with guidance from Stephen Grider's Udemy course.
 
-Next feature to be added: Server Side Architecture.
+Next feature to be added: MongoDB.
--- server/index.js
+++ server/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+//PassportJS Strategies
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            console.log( {accessToken, refreshToken, profile} );
+        }
+    )
+);
 
+//Route Handler – select aspects of user account via scope
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+//Server Ports
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT)
+    console.log('Server has started');
