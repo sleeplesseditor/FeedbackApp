19aff24f7f2f434f632264b82703eb74df1e58cb
sleeplesseditor <josh@bryson.me.uk>
2018-02-28 17:49:37 +0000

    user logout logic added

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -15,5 +15,14 @@ module.exports = (app) => {
             scope: ['profile', 'email']
         })
     );
-}
+
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
+};
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+//Use of MongoDB-generated user ID for multiple social media ID signin options
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
 //PassportJS Strategies
 passport.use(
     new GoogleStrategy(
@@ -14,7 +25,17 @@ passport.use(
             callbackURL: '/auth/google/callback'
         }, 
         (accessToken, refreshToken, profile, done) => {
-            new User({ googleId: profile.id }).save();
+            User.findOne({ googleId: profile.id }).then((existingUser) => {
+                    if (existingUser) {
+                        //Record with given profile ID exists
+                        done(null, existingUser);
+                    } else {
+                        //Record with given profile ID doesn't exist, make new record
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+            });
         }
     )
 );
