d96368f006a1702e5c23cb32009a75531beb86d4
sleeplesseditor <josh@bryson.me.uk>
2018-03-02 14:47:28 +0000

    First action creator created

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -4,7 +4,14 @@ class Header extends Component {
     render() {
         return(
             <div>
-                Header
+                  <nav>
+                    <div className="nav-wrapper">
+                        <a className="left brand-logo" href="#">Emaily</a>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            <li><a href="sass.html">Login with Google</a></li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         )
     }
