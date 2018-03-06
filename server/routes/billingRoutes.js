ece7fc527237eed01d4590c4e9765d780677ac85
sleeplesseditor <josh@bryson.me.uk>
2018-03-06 10:49:58 +0000

    header payment info complete

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,14 +1,22 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 //Creates charge to user credit card
 module.exports = (app) => {
-    app.post('/api/stripe', async (req, res) => {
+    app.post('/api/stripe', requireLogin, async (req, res) => {
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for 5 credits',
             source: req.body.id
         });
+
+        //Updates user credit balance
+        req.user.credits += 5;
+        //Updates user model
+        const user = await req.user.save();
+
+        res.send(user);
     });
 };
