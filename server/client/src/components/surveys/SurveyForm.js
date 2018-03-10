893a02a596d5850acd4dc514ec421245dd4a216f
sleeplesseditor <josh@bryson.me.uk>
2018-03-10 16:12:17 +0000

    Form routes updated

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -5,17 +5,11 @@ import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-    { label: 'Survey Title', name: 'title', noValueError: 'You must provide a survey title' }, 
-    { label: 'Subject Line', name: 'subject', noValueError: 'You must provide a subject' }, 
-    { label: 'Email Body', name: 'body', noValueError: 'You must provide body text' }, 
-    { label: 'Recipient List', name: 'emails', noValueError: 'You must provide a valid email address'}
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
     renderFields() {
-        return _.map(FIELDS, ({ label, name }) => {
+        return _.map(formFields, ({ label, name }) => {
             return ( 
                 <Field key={name} component={SurveyField} type="text" label={label} name ={name} />
             );
@@ -43,9 +37,9 @@ class SurveyForm extends Component {
 function validate(values) {
     const errors = {};
 
-    errors.emails = validateEmails(values.emails || '');
+    errors.recipients = validateEmails(values.recipients || '');
 
-    _.each(FIELDS, ({ name, noValueError }) => {
+    _.each(formFields, ({ name, noValueError }) => {
         if (!values[name]) {
             errors[name] = noValueError;
         }
@@ -56,5 +50,6 @@ function validate(values) {
 
 export default reduxForm({
     validate,
-    form: 'surveyForm'
+    form: 'surveyForm',
+    destroyOnUnmount: false
 })(SurveyForm);
---
+++ server/client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    { label: 'Survey Title', name: 'title', noValueError: 'You must provide a survey title' }, 
+    { label: 'Subject Line', name: 'subject', noValueError: 'You must provide a subject' }, 
+    { label: 'Email Body', name: 'body', noValueError: 'You must provide body text' }, 
+    { label: 'Recipient List', name: 'recipients', noValueError: 'You must provide a valid email address'}
+];
