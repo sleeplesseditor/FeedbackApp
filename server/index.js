ddf51d263f65179cdf415a48e077ed6dd543ea6d
sleeplesseditor <josh@bryson.me.uk>
2018-03-11 23:40:54 +0000

    Mongoose subdocument find and update feature updated

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
+mongoose.Promise = global.Promise;
 //Connection to remote-hosted database
 mongoose.connect(keys.mongoURI);
 
