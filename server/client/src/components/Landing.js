aae54a27ddb48f8bf9c74f07f05fb8e0c9a2e5b1
sleeplesseditor <josh@bryson.me.uk>
2018-03-12 00:53:46 +0000

    Landing Page Updated

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -4,7 +4,7 @@ const Landing = () => {
     return(
         <div style={{ textAlign: 'center' }}>
             <h1>
-                SurveyEmailHub
+                SurveyHub
             </h1>
             Collect feedback from your users
         </div>
